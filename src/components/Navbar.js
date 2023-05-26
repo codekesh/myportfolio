d297a8e37b5b91c29278b44cafc23e892cc07fb4
codekesh <201117@iiitt.ac.in>
2023-05-26 18:14:46 +0530

    Navabar edit and background change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,11 @@ import { NavLink } from "react-router-dom"
 import { Button } from '@mui/material';
 
 function Navbar() {
-    const logo = require('./images/profile.jpeg')
     const resume = "https://drive.google.com/drive/u/0/folders/11Ya3b47QkQeSx1OubBF3eQ_V7iykEj2e"
     return (
         <div className="navbar">
-            <NavLink to="/">
-                <img src={logo} alt="my-profile" />
-            </NavLink>
             <div className="nav-title">
+                <NavLink className="nav-link logo-name" to="/" > Keshav Tulsyan</NavLink>
                 <NavLink className="nav-link" to='/'>Home</NavLink>
                 <NavLink className="nav-link" to='projects' >Projects</NavLink>
                 <NavLink className="nav-link" to='work'>Work</NavLink>
